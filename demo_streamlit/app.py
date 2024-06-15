c82d27861e95c411335c437d298c29294d2e3197
Hisa <hisashi.konno@terragene.com>
2024-06-15 17:24:07 -0300

    correr el bot

--- demo_streamlit/app.py
+++ demo_streamlit/app.py
@@ -1,30 +1,41 @@
 import streamlit as st
 import requests
 
-# Clave de la API y URL
 API_KEY = "y7jjmBBP.pglw383yahVorfRBwK6Zo323dJ1lpnjN"
 URL = "https://payload.vextapp.com/hook/S590KL2AS8/catch/T-Assistant"
+
+# Clave de la API
+# API_KEY = "t3GtXbmk.bqLkYqNCB8dO9CnKHCbIfG6WcUXElTGb"
+
+# URL de la API
+# URL = "https://payload.vextapp.com/hook/Z4E7GGV354/catch/ASSISTANT"
 
 # Configuración de la página de Streamlit
 st.set_page_config(layout="wide")  # Establecer el layout de la página en ancho completo
 
 # Colocar el logo en la parte superior izquierda
-st.image("demo_streamlit/LOGOTIPO TERRAGENE Rev.1_Fondo Transparente-E.png", width=300)  # Ajusta el ancho según tus necesidades
+st.image("demo_streamlit/LOGOTIPO TERRAGENE Rev.1_Fondo Transparente-E.png", width=300)  # Ajusta el ancho según tus necesidades (doble del tamaño original de 150)
 st.title("Terragene Assistant")
 
 # Inicializar el estado de la conversación
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+# Mostrar los mensajes en el chat
+for message in st.session_state.messages:
+    st.write(message)
+
+# Entrada de texto para el mensaje del usuario
+user_input = st.text_input("Tú:", key="user_input")
+
 # Función para manejar el envío del mensaje
 def send_message():
-    user_input = st.session_state.user_input
     if user_input:
         # Agregar el mensaje del usuario a la conversación
         st.session_state.messages.append(f"Tú: {user_input}")
 
         # Hacer la solicitud a la API
-        headers = {"Content-Type": "application/json", "Api-Key": API_KEY}
+        headers = {"Content-Type": "application/json", "Apikey": f"Api-Key {API_KEY}"}
         data = {"payload": user_input}
         
         try:
@@ -41,14 +52,6 @@ def send_message():
         # Limpiar el campo de entrada
         st.session_state.user_input = ""
 
-# Mostrar los mensajes en el chat
-for message in st.session_state.messages:
-    st.write(message)
-
-# Entrada de texto para el mensaje del usuario
-st.text_input("Tú:", key="user_input")
-
 # Botón para enviar el mensaje
-if st.button("Enviar"):
-    send_message()
+if st.button("Enviar", on_click=send_message):
     st.experimental_rerun()
